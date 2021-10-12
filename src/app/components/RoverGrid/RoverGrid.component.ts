e5826067419811fbf880dace73d9f4dc69de45e5
AlyxMoon <allisterkmoon@gmail.com>
2021-10-12 16:21:10 -0700

    draw rover in the grid

--- src/app/components/RoverGrid/RoverGrid.component.ts
+++ src/app/components/RoverGrid/RoverGrid.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostBinding, Input } from '@angular/core'
 import { DomSanitizer } from '@angular/platform-browser'
+import { RoverPosition } from 'src/typings'
 
 @Component({
   selector: 'RoverGrid',
@@ -9,10 +10,32 @@ import { DomSanitizer } from '@angular/platform-browser'
 export class RoverGridComponent {
   @Input() width = 10
   @Input() height = 10
+  @Input() rover: RoverPosition = { x: 0, y: 0, d: 'N' }
 
   get cells (): number[] {
     const cells = Array(this.width * this.height).fill(0)
     return cells
+  }
+
+  get roverPositionIndex (): number {
+    // normally a grid would go top->bottom and left->right
+    // but we need bottom->top and left->right
+    // so a bit of translation is required
+
+    const { width, height } = this
+    const { x, y } = this.rover
+
+    return ((height - x - 1) * width) + y
+  }
+
+  get roverStyles (): { transform: string } {
+    let rotation = 0
+    if (this.rover.d === 'N') rotation = -90
+    if (this.rover.d === 'S') rotation = 90
+    if (this.rover.d === 'W') rotation = 180
+    if (this.rover.d === 'E') rotation = 0
+
+    return { transform: `rotateZ(${rotation}deg)` }
   }
 
   @HostBinding('attr.style')
